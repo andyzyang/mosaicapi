7f2aed0db11f58455dea0cded9544c608827c839
andyyang-zy <zongye.yang@mail.utoronto.ca>
2021-11-22 17:19:16 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# mosaicapi
+
+Describe your plan for scaling this service to support thousands of active users
+
+I would use caching to increase response speed, use load balancers and CDN to maximize throughput. Use HTTP/2 or HTTP/3 to reduce latency. Use a large-scale database like MongoDB with indexing.
---
+++ app.js
@@ -0,0 +1,110 @@
+const fs = require("fs");
+const datasetFile = __dirname + "/AI Dataset.json";
+const Datastore = require("nedb");
+
+let database = new Datastore({});
+
+const readFile = (filePath) => {
+  return new Promise(function (resolve, reject) {
+    fs.readFile(filePath, "utf8", function (err, data) {
+      if (err) return reject(err);
+      return resolve(data);
+    });
+  });
+};
+
+async function storeData() {
+  let data = await readFile(datasetFile);
+  let datasets = JSON.parse(data);
+  database.insert(datasets);
+}
+
+storeData().catch((err) => console.error(err));
+
+const express = require("express");
+const app = express();
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+app.use((req, res, next) => {
+  console.log("HTTP request", req.method, req.url, req.body);
+  next();
+});
+
+app.get("/api/projects/", (req, res, next) => {
+  let page = req.query.page;
+  let limit = req.query.limit || 10;
+  if (!page) {
+    page = 0;
+    limit = 0;
+  }
+  let queryObject = {
+    "Project School Name": req.query.psn || "",
+    "Project Description": req.query.pd || "",
+    "Project Phase Actual Start Date": req.query.ppasd || "",
+    "Project Phase Planned End Date": req.query.ppped || "",
+    "Project Phase Actual End Date": req.query.ppaed || "",
+    "Project Budget Amount": req.query.pba || "",
+    "Final Estimate of Actual Costs Through End of Phase Amount":
+      req.query.feactepa || "",
+    "Total Phase Actual Spending Amount": req.query.tpasa || "",
+  };
+
+  Object.entries(queryObject).forEach(([k, v]) => {
+    if (!v) delete queryObject[k];
+  });
+
+  console.log(queryObject);
+
+  database
+    .find(queryObject)
+    .skip(page * limit)
+    .limit(limit)
+    .exec(function (err, result) {
+      if (err) return res.status(500).end(err);
+      return res.json(result);
+    });
+});
+
+app.put("/api/projects/", function (req, res, next) {
+  if (!("_id" in req.body)) return res.status(400).end("_id is missing");
+  const id = req.body._id;
+  console.log(req.body);
+  let updateObject = req.body;
+  Object.entries(updateObject).forEach(([k, v]) => {
+    if (
+      k != "Project Phase Actual Start Date" &&
+      k != "Total Phase Actual Spending Amount"
+    )
+      delete updateObject[k];
+  });
+  console.log(updateObject);
+  if (updateObject != {}) {
+    database.findOne({ _id: id }, function (err, item) {
+      if (err) return res.status(500).end(err);
+      if (!item) {
+        return res.status(404).end("Project id #" + id + " does not exist");
+      }
+
+      database.update(
+        { _id: id },
+        { $set: updateObject },
+        { returnUpdatedDocs: true },
+        function (err, num, affectedDocument) {
+          res.json(affectedDocument);
+        }
+      );
+    });
+  }
+});
+
+const http = require("http");
+const PORT = 3000;
+
+http.createServer(app).listen(PORT, (err) => {
+  if (err) console.log(err);
+  else console.log("HTTP server on http://localhost:%s", PORT);
+});
+
+module.exports = app;
